e745d2a73469953b5b0c7284f788cd90953d5ef3
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2024-05-05 15:46:25 +0200

    Put into function

--- scripts/choropleth.py
+++ scripts/choropleth.py
@@ -1,32 +1,45 @@
 """Choropleth module."""
 
+from pathlib import Path
+
 import geopandas as gpd
 from shapely import geometry, ops, polygonize
 
-riksgrans = gpd.read_file("04_riksgrans.geojson", use_arrow=True, engine="pyogrio")
-sjograns = gpd.read_file(
-    "05_sjoterritoriets_grans_i_havet.geojson", use_arrow=True, engine="pyogrio"
-)
-lansgrans = gpd.read_file("02_lansgrans.geojson", use_arrow=True, engine="pyogrio")
-kommungrans = gpd.read_file("01_kommungrans.geojson", use_arrow=True, engine="pyogrio")
 
-# 21 regions
-lans = ops.linemerge(
-    geometry.MultiLineString(
-        sjograns.geometry.to_list()
-        + riksgrans.geometry.to_list()
-        + lansgrans.geometry.to_list()
+def extract_choropleth(path: str):
+    """Extract Choropleth polygons for regions and municipalities."""
+    path = Path(path)
+
+    riksgrans = gpd.read_file(
+        path / "04_riksgrans.geojson", use_arrow=True, engine="pyogrio"
     )
-)
-
-# 290 municipalities
-kommuns = ops.linemerge(
-    geometry.MultiLineString(
-        sjograns.geometry.to_list()
-        + riksgrans.geometry.to_list()
-        + lansgrans.geometry.to_list()
-        + kommungrans.geometry.to_list()
+    sjograns = gpd.read_file(
+        path / "05_sjoterritoriets_grans_i_havet.geojson",
+        use_arrow=True,
+        engine="pyogrio",
     )
-)
+    lansgrans = gpd.read_file(
+        path / "02_lansgrans.geojson", use_arrow=True, engine="pyogrio"
+    )
+    kommungrans = gpd.read_file(
+        path / "01_kommungrans.geojson", use_arrow=True, engine="pyogrio"
+    )
 
-len(polygonize(lans.geoms).geoms)
+    lans = ops.linemerge(
+        geometry.MultiLineString(
+            sjograns.geometry.to_list()
+            + riksgrans.geometry.to_list()
+            + lansgrans.geometry.to_list()
+        )
+    )
+
+    kommuns = ops.linemerge(
+        geometry.MultiLineString(
+            sjograns.geometry.to_list()
+            + riksgrans.geometry.to_list()
+            + lansgrans.geometry.to_list()
+            + kommungrans.geometry.to_list()
+        )
+    )
+
+    return lans, kommuns
